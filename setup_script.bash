a179cd534c2833ce2d3575d5d3debf2151acef97
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2014-10-29 14:55:08 -0400

    Added Setup script for creating a symbolic link to the mex file and create matlab root paths inside bashrc

---
+++ setup_script.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [[ "$#" -ne 2 ]] 
+	then
+	echo "Usage: setup_script.bash [MATLAB ROOT DIRECTORY: For example /usr/local/matlab/r2014a] [ROS_WORKSPACE: For example ~/hydro_workspace]"
+	exit
+fi
+PKG_FULL=`rospack find gazebo_rosmatlab_bridge`
+echo $PKG_FULL
+echo "export MATLAB_ROOT=$1">>~/.bashrc #Should be modified for each machine
+echo "export GAZEBO_MODEL_PATH=$PKG_FULL/models">>~/.bashrc 
+source ~/.bashrc
+source $2/devel/setup.bash
+roscd
+cd ..
+catkin_make
+cd devel/lib
+ln -s $PWD/mex_mmap.mexa64 $PKG_FULL/matlab_rosClient/mex_mmap.mexa64 
