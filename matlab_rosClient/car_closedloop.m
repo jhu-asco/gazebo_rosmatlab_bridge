0367753290b8a350ee8546d9e3cb85b554258041
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2015-03-30 15:01:19 -0400

    Added Servo support in car_closedloop

--- matlab_rosClient/car_closedloop.m
+++ matlab_rosClient/car_closedloop.m
@@ -9,9 +9,9 @@ S.sim = Gazebo_MatlabSimulator;%Creates a Matlab Bridge using a helper class
 S.sim.Configure(0.001,1);%Configure the physics engine to have a time step of 1 milli second and real time rate is 1
 
 %%% Problem Setting %%%%
-tf = 2;%Time to run the Controller for
-x0  = [0;0;0;0.5;0;0]; %Posn(x,y),Angle, Body Velocity of car, steering angle position, steering angle velocity
-S.xf = [2.5; 2; pi/2; 0.2];%Goal Posn(x,y),Angle, Body Velocity of car
+tf = 6;%Time to run the Controller for
+x0  = [0;0;0;0.02;0;0]; %Posn(x,y),Angle, Body Velocity of car, steering angle position, steering angle velocity
+S.xf = [2.5; -3; 0; 0.02];%Goal Posn(x,y),Angle, Body Velocity of car
 S.xi = x0(4);%Dynamic compensator for car controller 
 S.l = 0.5; %Length of the car
 frequency = 50;%Hz
@@ -23,6 +23,12 @@ S.nofsteps = round(tf/(S.N*S.sim.physxtimestep));%Internal computation of nof ph
 %Reset the Physics World
 mex_mmap('reset',S.sim.Mex_data);
 pause(0.01);% Wait for it take effect may not be necessary
+
+%Attach servo to all the joints:
+mex_mmap('attachservo',S.sim.Mex_data, 0,[2.0;0.1;0.0],[2;-2;5;-5],1)
+mex_mmap('attachservo',S.sim.Mex_data, 2,[2.0;0.1;0.0],[2;-2;5;-5],1)
+mex_mmap('attachservo',S.sim.Mex_data, 1,[100.0;20.0;100.0],[20;-20;100;-100])
+mex_mmap('attachservo',S.sim.Mex_data, 3,[100.0;20.0;100.0],[20;-20;100;-100])
 
 %Set car to initial posn:
 modelposeandtwist = [x0(1:2)' 0.05 rpy2quat([x0(3), 0, 0])' x0(4) zeros(1,5)];%13x1
@@ -61,7 +67,7 @@ function [u,S] = CarController(x,S)
     %car
     %Output of the car is the position of wheels between axis:
     %Gains for virtual inputs:
-    kpv = 1.0;
+    kpv = 0.2;
     kpvd = 1.0;
     y = x(1:2);
     ydot = [x(4)*cos(x(3)); x(4)*sin(x(3))];
@@ -78,14 +84,27 @@ function [u,S] = CarController(x,S)
     temp = B*v;%temp is xidot(u1dot) and u2
     S.xi = temp(2)*S.h + S.xi;%Integrate the compensator 
     phi = atan(temp(1));%Desired Steering angle
-    %Use PD Controllers to achieve desired steering angle and desired
-    %velocity:
-    u(1,1) = 5.0*(S.xi - x(4));%drivetorque = kp *error_velocity (P)
-    u(2,1) = 1.0*(phi - x(5)) + 1.0*(-x(6));%steeringtorque using pid
-    disp('x: ');
-    disp(x);
-    disp('u: ');
-    disp(u);
+    %Bind steering angle:
+    if (phi > pi/4)
+        phi = pi/4;
+    elseif (phi < -pi/4)
+        phi = -pi/4;
+    end
+    disp('steer: ');
+    disp(phi);
+    disp('xbar: ');
+    disp(atan2(v(2),v(1)));
+    u = [20*S.xi; phi];%20 is the conversion from body velocity to joint velocity
+    %Since servos are attached to joints the commands are directly velocity
+    %and steering angle
+%     %Use PD Controllers to achieve desired steering angle and desired
+%     %velocity:
+%     u(1,1) = 5.0*(S.xi - x(4));%drivetorque = kp *error_velocity (P)
+%     u(2,1) = 1.0*(phi - x(5)) + 1.0*(-x(6));%steeringtorque using pid
+%     disp('x: ');
+%     disp(x);
+%     disp('u: ');
+%     disp(u);
 
 
 
