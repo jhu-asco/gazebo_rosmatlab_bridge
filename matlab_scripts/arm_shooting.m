80de17596c959491989a5f32c482828e74b7c9d5
garimella.gowtham74@gmail.com <garimella.gowtham74@gmail.com>
2015-11-12 12:05:48 -0500

    Fixed some obsolete code in arm_shooting

--- matlab_scripts/arm_shooting.m
+++ matlab_scripts/arm_shooting.m
@@ -25,9 +25,8 @@ S.f = @arm_f;
 
 S.sim = GazeboMatlabSimulator;%Creates a Matlab Bridge using a helper class
 S.sim.Configure(0.001,100);%Configure the physics engine to have a time step of 1 milli second and real time rate is 100
-S.steps = uint32(round((0:S.h:tf)/S.sim.PhysicsTimeStep));%Converts the time into physics time steps
-% For example if the open loop trajectory is 1 second with 4 segments then the time is [0, 0.25, 0.5, 0.75, 1] and the steps are [0 250 500 750 1000] 
-% since physics time step is 1 millisecond
+S.sim.ActuatedJoints = [1 2]; %These are the joints that are being actuated
+S.ts = 0:S.h:tf;%Time steps
 
 % initial state
 x0 = [-1; 0; 0; 0;];%State is Joint angles(1,2) and Joint Velocities(1,2)
@@ -48,20 +47,20 @@ xs = zeros(4,S.N+1);
 figure(1),clf,
 S.phandle(1) = plot(0,0);
 hold on, S.phandle(2) = plot(0,0,'r');
-set(S.phandle(1),'XData',S.steps);
+set(S.phandle(1),'XData',S.ts);
 set(S.phandle(1),'YDataSource','xs(1,:)');
-set(S.phandle(2),'XData',S.steps);
+set(S.phandle(2),'XData',S.ts);
 set(S.phandle(2),'YDataSource','xs(2,:)');
 figure(2),clf,
 S.phandle(3) = plot(0,0);
 hold on, S.phandle(4) = plot(0,0,'r');
-set(S.phandle(3),'XData',(S.steps(1:end-1)));
+set(S.phandle(3),'XData',(S.ts(1:end-1)));
 set(S.phandle(3),'YDataSource','us(1,:)');
-set(S.phandle(4),'XData',(S.steps(1:end-1)));
+set(S.phandle(4),'XData',(S.ts(1:end-1)));
 set(S.phandle(4),'YDataSource','us(2,:)');
 hold on
 
-m = size(us,1);
+
 N = S.N;
 lb=repmat([-50; -50], N, 1); % can incorporate upper and lower bound
 ub=repmat([50; 50], N, 1);
@@ -71,15 +70,23 @@ us = lsqnonlin(@(us)arm_cost(us, S), us, lb, ub,options);
 % Display final control and state information
 disp('us');
 disp(us);
-% % update trajectory
-xs = sys_traj(x0, us, S);
-% 
+while (1)
+    user_input = input('Press (a) to animate; (q) to quit;\n','s');
+    if user_input == 'a'
+        S.sim.Configure(0.001,1);
+        % % update trajectory
+        xs = sys_traj(x0, us, S);
+    elseif user_input == 'q'
+        break;
+    end
+end
+
 disp('xs');
 disp(xs);
 
 
 function y = arm_cost(us, S)
-% this the arm costs in least-squares form,
+% This evaluates the arm costs in least-squares form,
 % i.e. the residuals at each time-step
 
 us = reshape(us, S.m, S.N);
@@ -104,22 +111,20 @@ disp(0.5*y'*y);
 function xs = sys_traj(x0, us, S)
 
 N = size(us, 2);
-jointids = [1 2];
 %xs(:,1) = x0;
-mex_mmap('reset',S.sim.MexData);
+S.sim.Reset;
 pause(0.01);
 
-mex_mmap('setmodelstate',S.sim.MexData,'double_pendulum_with_base',[],...
-    uint32(jointids)-1,[x0(1:2);x0(3:4)]); %Set the initial Joint angles
+S.sim.SetModelState('double_pendulum_with_base',[],[x0(1:2)';x0(3:4)']);%Set the model to the initial state
 
-[~, JointData] = mex_mmap('runsimulation',S.sim.MexData, uint32(jointids)-1, us, ...
-                                                    [], [], S.steps);% Run the simulation and collect the joint angles and velocities along the trajectory
+[~, JointData] = S.sim.StepTrajectory(S.ts, us);%Run simulation for gazebo simulation for specified ts and collect joint data along the way
+
 xs([1,3],1:(N+1)) = JointData(:,1:2:(2*(N+1)));
 xs([2,4],1:(N+1)) = JointData(:,2:2:(2*(N+1)));
 %Map the angles to -pi to pi
 A = rem(xs(1:2,:),2*pi);
 A(A>pi) = A(A>pi)-2*pi;
-A(A<-pi) = A(A<-pi)+2*pi;
+A(A<-pi) = A(A<-phttp://www.mathworks.com/help/optim/ug/lsqnonlin.htmli)+2*pi;
 xs(1:2,:)= A;
 % Check if initial state matches with S.x0
 if norm(xs(:,1) - S.x0(:,1))>1e-3
